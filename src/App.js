a252cd3b56618ef8226f000c93dad19c9460bea5
K1ngQ1 <wigginsquinten@gmail.com>
2022-05-26 17:25:42 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,111 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import './App.css'
+import SingleCard from './components/SingleCard'
 
-function App() {
+
+const cardImages = [
+  {"src": "/img/1.png", matched: false},
+  {"src": "/img/2.png", matched: false},
+  {"src": "/img/3.png", matched: false},
+  {"src": "/img/4.png", matched: false},
+  {"src": "/img/5.png", matched: false},
+  {"src": "/img/6.png", matched: false}
+]
+
+
+export default function App() {
+
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0)
+  const [choiceOne, setChoiceOne] = useState(null)
+  const [choiceTwo, setChoiceTwo] = useState(null)
+  const [disabled, setDisabled] = useState(false)
+
+  //shuffle cards, gives random id and duplicates
+  const shuffleCards = () => {
+    const shuffleCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }))
+
+      setChoiceOne(null)
+      setChoiceTwo(null)
+      setCards(shuffleCards)
+      setTurns(0)
+  }
+
+  
+  //handle a choice
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+  }
+
+
+  //compare cards
+  useEffect(() =>{
+    if(choiceOne != null && choiceTwo != null){
+      setDisabled(true)
+      if(choiceOne.src === choiceTwo.src){
+        setCards(prevCards => {
+          return prevCards.map(card => {
+            if (card.src === choiceOne.src){
+              return {...card, matched: true}
+            }else{
+              return card
+            }
+          })
+        })
+        resetTurn()
+      }else{
+        
+        setTimeout(() => resetTurn(), 1000)}
+    }
+  }, [choiceOne, choiceTwo])
+
+  //info alert
+
+  const introAlert = () => {
+    alert(
+      `Welcome to my game!`
+      )
+  }
+
+
+  // reset choices and adds a turn
+  const resetTurn = () => {
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setTurns(prevTurns => prevTurns + 1)
+    setDisabled(false)
+  }
+  
+  // start game auto
+
+  useEffect(() => {
+    shuffleCards()
+    introAlert()
+  },[])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='main'>
+      <h1>Match the cards!</h1>
+      <p>Turns: {turns}</p>
+      <div className='button-grid'>
+      <button onClick={shuffleCards}>New Game</button>
+      <button>Help Me!</button>
+      </div>
+      <div className='card-grid'>
+        {cards.map(card => (
+            <SingleCard 
+              key={card.id} 
+              card={card}
+              handleChoice={handleChoice}
+              flipped={card === choiceOne || card === choiceTwo || card.matched}
+              disabled ={disabled}
+            />
+        ))}
+      </div>
+          
     </div>
-  );
+  )
 }
-
-export default App;
